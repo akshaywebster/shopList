9b8d3195dd698232504b680b121d06d9aed31111
Akshay Chaudhary <akshaych49@gmail.com>
2022-03-09 14:14:53 +0530

    fixed component re-render bug

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({ type, msg, removeAlert }) => {
+const Alert = ({ type, msg, removeAlert, list }) => {
   useEffect(() => {
     const timeOut = setTimeout(() => {
       removeAlert()
     }, 3000)
     return () => clearTimeout(timeOut)
-  }, [])
+  }, [list]) // re-render component every time list changes instead of fixed 3 sec
 
   return <p className={`alert alert-${type}`}>{msg}</p>
 }
